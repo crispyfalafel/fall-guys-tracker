67187848abd1c415e3589d32d5c99c73b29ff37b
Jolin Lin <61108483+crispyfalafel@users.noreply.github.com>
2020-08-29 04:06:57 +0000

    first commit

---
+++ helpers.py
@@ -0,0 +1,44 @@
+import os
+import requests
+import urllib.parse
+
+from flask import redirect, render_template, request, session
+from functools import wraps
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [("-", "--"), (" ", "-"), ("_", "__"), ("?", "~q"),
+                         ("%", "~p"), ("#", "~h"), ("/", "~s"), ("\"", "''")]:
+            s = s.replace(old, new)
+        return s
+    return render_template("apology.html", top=code, bottom=escape(message)), code
+
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
+def percent(n, m):
+    """
+    Calculate percentage of n in m, then convert to string percentage format with 2 decimals
+    """
+
+    if n == 0 or m == 0:
+        return "0.00%"
+    percent = n/m * 100
+    return "{:,.2f}%".format(percent)
